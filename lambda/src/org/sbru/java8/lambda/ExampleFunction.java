349adc230b667d5778d10f728c41147dc7686eee
Salvabb81 <salvabb81@users.noreply.github.com>
2020-12-03 12:59:09 +0100

    Example lambda function

--- lambda/src/org/sbru/java8/lambda/ExampleFunction.java
+++ lambda/src/org/sbru/java8/lambda/ExampleFunction.java
@@ -6,27 +6,26 @@ import java.util.function.Function;
 public class ExampleFunction {
 
 	public static void main(String[] args) {
-		
+
 		Function<String, String> f1 = param -> "Hello how are you? " + param;
 		String result = f1.apply("Salvador");
 		System.out.println(result);
-		
-		
+
 //		Function<String, String> f2 = param -> param.toUpperCase();
 		Function<String, String> f2 = String::toUpperCase;
 		System.out.println(f2.apply("Salvador"));
-		
+
 		BiFunction<String, String, String> f3 = (a, b) -> a.toUpperCase().concat(b.toUpperCase());
 		String r2 = f3.apply("Salvador", "Angel");
 		System.out.println(r2);
-		
+
 //		BiFunction<String, String, Integer> f4 = (a, b) -> a.compareTo(b);
 		BiFunction<String, String, Integer> f4 = String::compareTo;
 		System.out.println(f4.apply("Salvador", "Salvador"));
-		
+
 //		BiFunction<String, String, String> f5 = (a, b) -> a.concat(b);
 		BiFunction<String, String, String> f5 = String::concat;
 		System.out.println(f5.apply("Salvador", "Angel"));
 	}
-	
+
 }
