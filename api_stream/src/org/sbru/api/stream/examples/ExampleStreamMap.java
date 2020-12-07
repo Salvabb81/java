5f7a3b3898822dece4b1a1f6dd1bf6a782a0876d
Salvabb81 <salvabb81@users.noreply.github.com>
2020-12-07 10:23:23 +0100

    Map operator

--- api_stream/src/org/sbru/api/stream/examples/ExampleStreamMap.java
+++ api_stream/src/org/sbru/api/stream/examples/ExampleStreamMap.java
@@ -1,10 +1,10 @@
 package org.sbru.api.stream.examples;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import org.sbru.api.stream.examples.models.User;
 
 public class ExampleStreamMap {
 
@@ -17,6 +17,18 @@ public class ExampleStreamMap {
 		List<String> list = names.collect(Collectors.toList());
 		list.forEach(System.out::println);
 //		names.forEach(System.out::println);
+		
+		Stream<User> names2 = Stream
+				.of("Salvador Bru", "John Doe", "Aimee Yao", "Maria Smith")
+				.map(firstName -> new User(firstName.split(" ")[0], firstName.split(" ")[1]))
+				.peek(System.out::println)
+				.map(user -> {
+					 String firstName = user.getFirstName().toUpperCase();
+					user.setFirstName(firstName);
+					return user; 
+				});
+		List<User> list2 = names2.collect(Collectors.toList());
+		list2.forEach(System.out::println);
 
 	}
 }
