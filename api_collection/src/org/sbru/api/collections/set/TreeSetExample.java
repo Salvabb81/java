a24cc17d7ae337893bcfaef56019391275adb068
Salvabb81 <salvabb81@users.noreply.github.com>
2021-01-25 13:10:11 +0100

    TreeSet example

---
+++ api_collection/src/org/sbru/api/collections/set/TreeSetExample.java
@@ -0,0 +1,31 @@
+package org.sbru.api.collections.set;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetExample {
+
+	public static void main(String[] args) {
+
+		Set<String> ts = new TreeSet<>((a, b) -> b.compareTo(a));
+
+		ts.add("uno");
+		ts.add("dos");
+		ts.add("tres");
+		ts.add("tres");
+		ts.add("cuatro");
+		ts.add("cinco");
+
+		System.out.println("ts = " + ts);
+
+		Set<Integer> numeros = new TreeSet<>(Comparator.reverseOrder());
+		numeros.add(5);
+		numeros.add(3);
+		numeros.add(1);
+		numeros.add(2);
+		numeros.add(4);
+
+		System.out.println("numeros = " + numeros);
+	}
+}
