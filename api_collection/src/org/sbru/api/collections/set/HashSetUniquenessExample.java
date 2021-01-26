cb0d8ebb1602dab07aef33c314c2db547ab10a72
Salvabb81 <salvabb81@users.noreply.github.com>
2021-01-26 11:54:33 +0100

    Iteration examples with Lists and Sets

--- api_collection/src/org/sbru/api/collections/set/HashSetUniquenessExample.java
+++ api_collection/src/org/sbru/api/collections/set/HashSetUniquenessExample.java
@@ -1,6 +1,9 @@
 package org.sbru.api.collections.set;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.sbru.api.collections.modelo.Alumno;
@@ -10,6 +13,7 @@ public class HashSetUniquenessExample {
 	public static void main(String[] args) {
 		
 		Set<Alumno> sa = new HashSet<>();
+		List<Alumno> sa2 = new ArrayList<>();
 		sa.add(new Alumno("Salvador", 5));
 		sa.add(new Alumno("Aimee", 6));
 		sa.add(new Alumno("John", 4));
@@ -19,5 +23,25 @@ public class HashSetUniquenessExample {
 		sa.add(new Alumno("Jack", 2));
 		
 		System.out.println(sa);
+		
+		System.out.println("Iterating with normal for:");
+		for (int i = 0; i < sa.size(); i++) {
+			System.out.println(sa2.get(i));
+		}
+		
+		System.out.println("Iterating using foreach:");
+		for(Alumno a: sa) {
+			System.out.println(a);
+		}
+		
+		System.out.println("Iterating using while and iterator:");
+		Iterator<Alumno> it = sa.iterator();
+		while(it.hasNext()) {
+			Alumno a = it.next();
+			System.out.println(a.getNombre());
+		}
+		
+		System.out.println("Iterating using Stream foreach:");
+		sa.forEach(System.out::println);
 	}
 }
