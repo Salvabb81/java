13ab569fe9fe957861dc94987280ba05e566e0f9
Salvabb81 <salvabb81@users.noreply.github.com>
2021-01-27 11:11:17 +0100

    LinkedList example

---
+++ api_collection/src/org/sbru/api/collections/set/LinkedListExample.java
@@ -0,0 +1,50 @@
+package org.sbru.api.collections.set;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.sbru.api.collections.modelo.Alumno;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+
+		LinkedList<Alumno> linked = new LinkedList<>();
+		System.out.println(linked + ", size = " + linked.size());
+		System.out.println("Is empty: " + linked.isEmpty());
+		linked.add(new Alumno("Salvador", 5));
+		linked.add(new Alumno("Aimee", 6));
+		linked.add(new Alumno("John", 4));
+		linked.add(new Alumno("Peter", 7));
+		linked.add(new Alumno("Lucy", 3));
+
+		System.out.println(linked + ", size = " + linked.size());
+
+		linked.addFirst(new Alumno("Rodrigo", 5));
+		linked.addLast(new Alumno("Jack", 6));
+		System.out.println(linked + ", size = " + linked.size());
+
+		System.out.println("Primero = " + linked.getFirst());
+		System.out.println("Primero = " + linked.peekFirst());
+		System.out.println("Ultimo = " + linked.getLast());
+		System.out.println("Ultimo = " + linked.peekLast());
+		System.out.println("Indice 2 = " + linked.get(2));
+		Alumno rodrigo = linked.removeFirst();
+//		linked.pop();
+		linked.removeLast();
+//		 linked.pollLast();
+		System.out.println(linked + ", size = " + linked.size());
+
+		linked.remove(new Alumno("John", 4));
+		System.out.println(linked + ", size = " + linked.size());
+
+		Alumno a = new Alumno("Lucas", 9);
+		linked.addLast(a);
+		System.out.println("Indice de Lucas = " + linked.indexOf(a));
+		linked.remove(2);
+		System.out.println(linked + ", size = " + linked.size());
+
+		linked.set(3, new Alumno("Ryan", 8));
+		System.out.println(linked + ", size = " + linked.size());
+	}
+}
